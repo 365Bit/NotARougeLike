28e464d236200024e9306b187d05f3cc846fe8e2
David Ruff <david.ruff@live.de>
2025-12-09 19:08:26 +0100

    set savegame path

--- Assets/Scripts/GameSaver.cs
+++ Assets/Scripts/GameSaver.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 class GameSaver
 {
     private static List<WeakReference> saveables = new List<WeakReference>();
+    private static string savePath;
+
+    static GameSaver()
+    {
+        var home=Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            savePath=Path.Combine(new string[]{home,"Saved Games","NotARogueLike"});
+        }else if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            savePath=Path.Combine(new string[]{home,".local","share","not_a_rogue_like"});
+        }
+        else
+        {
+            throw new NotImplementedException("There is no savegame location implemented for the current operating system.\nSupported are: Linux and MS Windows");
+        }
+
+    } 
     public static void subscribe<T>(T self)
     {
         var reference = new WeakReference(self);
