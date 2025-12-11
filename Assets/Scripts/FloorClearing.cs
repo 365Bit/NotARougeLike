75a4b27a022988a7a8677e242a209f55714762b4
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-11 18:59:40 +0100

    add script that checks clearing condition

---
+++ Assets/Scripts/FloorClearing.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorClearing : MonoBehaviour
+{
+    // reduce Find calls
+    private GameObject cachedOpponent;
+
+    public bool CheckClearingCondition() {
+        if (cachedOpponent != null && cachedOpponent.activeInHierarchy) return false;
+
+        cachedOpponent = GameObject.Find("Opponent(Clone)");
+        return cachedOpponent == null;
+    }
+
+    void Update()
+    {
+        if (CheckClearingCondition()) {
+            Debug.Log("Floor cleared, going to next level");
+            RunData.Instance.level++;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
