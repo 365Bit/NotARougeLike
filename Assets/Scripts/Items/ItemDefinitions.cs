67777d14d6cecfa7c3deadeb5dc77e6e674b9ef3
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-11 19:00:14 +0100

    remove unnecessary enum

--- Assets/Scripts/Items/ItemDefinitions.cs
+++ Assets/Scripts/Items/ItemDefinitions.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 
-public enum ItemType {
-    manaFruit,
-    healthFruit,
-    staminaFruit
-}
-
 public enum Currency {
     Gold = 0
 }
