7d68ca6798adf95de12337d18ad1107da3ff781b
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-13 12:41:47 +0100

    add constants for initial currencies and items

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,4 +4,9 @@ public class Constants : MonoBehaviour
 {
     public int itemSlots;
     public int hotbarSlots;
+
+    // definitions for new run
+    public int initialGold;
+    public int initialXP;
+    public int initialAmmo;
 }
