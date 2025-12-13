ffa71a3e0294a3ddb56930c0f2b007e6220898ca
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-13 16:31:14 +0100

    add scripts for upgrade ui

---
+++ Assets/Scripts/UI/StatDisplay.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+
+using UnityEngine.UI;
+using TMPro;
+
+public class StatDisplay : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text levelText;
+    public Button upgradeButton;
+
+    public Color background, selectedBackground;
+
+    public void SetStat(BaseStatKey stat, int level) {
+        nameText.text  = Enum.GetName(typeof(BaseStatKey), stat);
+        levelText.text = level.ToString();
+    }
+
+    public void SetSelected(bool selected) {
+        GetComponent<Image>().color = selected ? selectedBackground : background;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        GetComponent<Image>().color = background;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/UpgradeUI.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System;
+
+public class UpgradeUI : MonoBehaviour
+{
+    public GameObject statUIPrefab;
+
+    public Vector2 origin;
+    public Vector2 offset;
+
+    private int selectedStat;
+
+    private GameObject[] uiInstances;
+
+    // get array of stats
+    private Array stats = Enum.GetValues(typeof(BaseStatKey));
+
+    // 
+    public void MoveSelection(int delta) {
+        selectedStat = (selectedStat + delta + stats.Length) % stats.Length;
+    }
+
+    public bool TryUpgrade() {
+        Debug.Log("trying to upgrade " + Enum.GetName(typeof(BaseStatKey), stats.GetValue(selectedStat)));
+        return false;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        // clear
+        if (uiInstances != null) 
+            foreach (var i in uiInstances) 
+                GameObject.Destroy(i);
+        else
+            uiInstances = new GameObject[stats.Length];
+
+        int index = 0;
+        foreach(BaseStatKey key in stats) {
+            var obj = Instantiate(statUIPrefab, transform);
+            obj.transform.localPosition = origin + offset * index;
+
+            var disp = obj.GetComponent<StatDisplay>();
+            disp.SetStat(key, 0);
+            disp.SetSelected(false);
+
+            uiInstances[index] = obj;
+            index++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = 0;
+        foreach (GameObject i in uiInstances) {
+            i.GetComponent<StatDisplay>().SetSelected(index == selectedStat);
+            index++;
+        }
+    }
+}
