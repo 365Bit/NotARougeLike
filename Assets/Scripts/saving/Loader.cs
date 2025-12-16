d70704a870b6d774c623999b51a3a1581b771d72
ruffdd <ruffdd@studi.informatik.uni-stuttgart.de>
2025-12-16 17:26:37 +0100

    fix parsing errors

--- Assets/Scripts/saving/Loader.cs
+++ Assets/Scripts/saving/Loader.cs
@@ -18,10 +18,17 @@ class Loader
     {
         var content = new StringBuilder((int)new FileInfo(path).Length);
         reader = new StreamReader(path);
+        bool inString=false;
         for (int read =' '; (read=reader.Read()) >= 0;)
         {
             char c = (char)read;
-            if (c == ' ' || c == '\t' || c =='\r' || c=='\n')
+            if (c == '\\' &&inString)
+            {
+                read=reader.Read();
+            }else if (c == '\"')
+            {
+                inString=!inString;
+            }else if ((c == ' ' || c == '\t' || c =='\r' || c=='\n')&&!inString)
             {
                 continue;
             }
@@ -36,7 +43,7 @@ class Loader
         if (data.First() == '{' && data.Last() == '}')
         {
             var dict = new Dictionary<string, LoadedItem>();
-            foreach (var line in data.Substring(1, data.Length - 1).Split(','))
+            foreach (var line in splitList(data.Substring(1, data.Length - 1)))
             {
                 var item = splitTuple(line);
                 dict.Add(item.Item1,load(item.Item2));
@@ -46,7 +53,7 @@ class Loader
         else if (data.First() == '[' && data.Last() == ']')
         {
             var list = new List<LoadedItem>();
-            foreach (var item in data.Substring(1, data.Length - 1).Split(','))
+            foreach (var item in splitList(data.Substring(1, data.Length - 1)))
             {
                 list.Add(load(item));
             }
@@ -80,10 +87,55 @@ class Loader
                 inString=!inString;
             }else if (!inString && tuple[i] == ':')
             {
-                return new Tuple<string, string>(tuple.Substring(0,i),tuple.Substring(i+1));
+                return new Tuple<string, string>(tuple.Substring(0+1,i-2),tuple.Substring(i+2));
             }
         }
         throw new LoadException($"The string\"{tuple}\" cannot be loaded as a tuple. No colon was found");
+    }
+
+    private List<string> splitList(string data)
+    {
+        List<string> output = new List<string>();
+        int laststart=0;
+        bool inString=false;
+        int dict=0;
+        int list=0;
+        for(int i = 0; i < data.Length; i++)
+        {
+            if (data[i] == '\\')
+            {
+                i++;
+                continue;
+            }else if (data[i] == '"')
+            {
+                inString=!inString;
+            }else if (inString)
+            {
+                continue;
+            }else if (data[i] == '{')
+            {
+                dict++;
+            }else if (data[i] == '}')
+            {
+                dict--;
+            }else if (data[i] == '[')
+            {
+                list++;
+            }else if (data[i] == ']')
+            {
+                list--;
+            }else if (data[i] == ','&&dict==0&&list==0)
+            {
+                output.Add(data.Substring(laststart,i-laststart-1));
+                laststart=i+1;
+            }
+            else
+            {
+                
+            }
+        }
+        output.Add(data.Substring(laststart));
+        return output;
     }
 
 }
