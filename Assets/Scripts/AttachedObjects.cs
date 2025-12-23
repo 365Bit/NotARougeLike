bc8de6112cd9f781b5f4df8544af455369f38bc3
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-23 12:59:14 +0100

    split attached objects into base and derived class

--- Assets/Scripts/AttachedObjects.cs
+++ Assets/Scripts/AttachedObjects.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
 
+// class for managing objects that are temporarily attached to others (e.g. projectiles that have hit something)
 public class AttachedObjects : MonoBehaviour
 {
+    // ring buffer size
     public int maximumAttachedObjects = 10;
-    public SkinnedMeshRenderer mesh;
- 
-    // 
-    private int index = 0;
-    private Transform[] bones;
-    private Transform[] attachedObjects;
-    private Vector3[] offsets;
-    private Quaternion[] rotations;
- 
-    bool visible = true;
+
+    // information for each attached object
+    protected int index = 0;
+    protected Transform[] attachmentTargets;
+    protected Transform[] attachedObjects;
+    protected Vector3[] offsets;
+    protected Quaternion[] rotations;
+
+    // only update positions when visible
+    protected bool visible = true;
  
     void OnBecameInvisible() {
         visible = false;
@@ -21,74 +23,77 @@ public class AttachedObjects : MonoBehaviour
     void OnBecameVisible() {
         visible = true;
     }
- 
-    void Start() {
-        bones = new Transform[maximumAttachedObjects];
+
+    void Update() {
+        if (!visible) return;
+
+        UpdatePositions();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AttachedObjects()
+    {
         attachedObjects = new Transform[maximumAttachedObjects];
- 
+        attachmentTargets = new Transform[maximumAttachedObjects];
         offsets = new Vector3[maximumAttachedObjects];
         rotations = new Quaternion[maximumAttachedObjects];
     }
- 
-    public void Attach(Transform obj) {
-        if (mesh == null) return;
 
-        Transform bone = mesh.bones[0];
-        float sqrDist = float.MaxValue;
-        foreach (var b in mesh.bones) {
-            float d = b.InverseTransformPoint(obj.position).sqrMagnitude;
-            if (d < sqrDist) {
-                bone = b;
-                sqrDist = d;
-            }
-        }
+    public virtual void Attach(Transform obj) {
+        AttachTo(obj, transform);
+    }
 
+    protected int AttachTo(Transform obj, Transform target) {
         // if overwriting, detach first
         if (attachedObjects[index] != null)
             OnDetach(attachedObjects[index]);
 
         attachedObjects[index] = obj;
-        bones[index] = bone;
+        attachmentTargets[index] = target;
 
-        offsets[index] = bone.InverseTransformPoint(obj.position);
-        rotations[index] =  Quaternion.Inverse(bone.rotation) * obj.rotation;
+        offsets[index] = target.InverseTransformPoint(obj.position);
+        rotations[index] =  Quaternion.Inverse(target.rotation) * obj.rotation;
 
         // call attachement function
         OnAttach(attachedObjects[index]);
 
+        int result = index;
         index = (index + 1) % maximumAttachedObjects;
+        return result;
     }
 
-    void OnAttach(Transform obj) {
+    protected void UpdatePositions() {
+        for (int i = 0; i < maximumAttachedObjects; i++) {
+            if (attachedObjects[i] == null || attachmentTargets[i] == null) continue;
+
+            attachedObjects[i].SetPositionAndRotation(attachmentTargets[i].TransformPoint(offsets[i]),  attachmentTargets[i].rotation * rotations[i]);
+        }
+    }
+
+    protected void DetachAll() {
+        // update positions now if not done during update function
+        if (!visible) UpdatePositions();
+
+        for (int i = 0; i < maximumAttachedObjects; i++) {
+            if (attachedObjects[i] == null || attachmentTargets[i] == null) continue;
+
+            OnDetach(attachedObjects[i]);
+        }
+    }
+
+    void OnDestroy() {
+        DetachAll();
+    }
+
+    protected void OnAttach(Transform obj) {
 	    if (obj.TryGetComponent<Attachable>(out Attachable a)) {
 	    	a.OnAttach(this);
 	    }
     }
 
-    void OnDetach(Transform obj) {
+    protected void OnDetach(Transform obj) {
 	    if (obj.TryGetComponent<Attachable>(out Attachable a)) {
 	    	a.OnDetach();
 	    }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!visible) return;
-
-        for (int i = 0; i < maximumAttachedObjects; i++) {
-            if (attachedObjects[i] == null || bones[i] == null) continue;
-
-            attachedObjects[i].SetPositionAndRotation(bones[i].TransformPoint(offsets[i]),  bones[i].rotation * rotations[i]);
-        }
-    }
-
-    void OnDestroy() {
-        for (int i = 0; i < maximumAttachedObjects; i++) {
-            if (attachedObjects[i] == null || bones[i] == null) continue;
-
-            OnDetach(attachedObjects[i]);
-        }
-    }
 }
-
