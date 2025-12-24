25a40b509282de5be09d8e364aab9fe6b1b14248
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-24 13:15:58 +0100

    create script for dungeon property scaling

---
+++ Assets/Scripts/DungeonGeneration/DungeonPropertyDefinitions.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class DungeonPropertyDefinitions : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Def {
+        public DungeonPropertyKey key;
+        public InterpolationScaling def;
+    };
+
+    // for editor
+    [SerializeField]
+    private Def[] definitions;
+
+    // internal data structure
+    private InterpolationScaling[] _defs;
+
+    // put definitions into an array
+    void Awake() {
+        _defs = new InterpolationScaling[Enum.GetValues(typeof(DungeonPropertyKey)).Length];
+        foreach (Def d in definitions) {
+            _defs[(int)d.key] = d.def;
+        }
+
+        // check definitions
+        for (int i = 0; i < _defs.Length; i++) {
+            if (_defs[i] == null) Debug.Log("scaling undefined for " + Enum.GetName(typeof(DungeonPropertyKey), (DungeonPropertyKey) i));
+        }
+    }
+
+    // returns the dungeon properties for the given level
+    public Dictionary<DungeonPropertyKey, float> 
+    ComputeFrom(int level) {
+        Dictionary<DungeonPropertyKey, float> result = new();
+        
+        for (int i = 0; i < _defs.Length; i++) {
+            result.Add((DungeonPropertyKey) i, _defs[i].ComputeFrom(level));
+        }
+
+        return result;
+    }
+}
+
+[System.Serializable]
+public class InterpolationScaling {
+    [System.Serializable]
+    public struct Point {
+        public int   level;
+        public float value;
+    }
+
+    // interpolation points
+    public Point[] points;
+
+    public float ComputeFrom(int level) {
+        int index = 0;
+        for (; index + 1 < points.Length && points[index + 1].level <= level; index++);
+
+        // index+1 still has to point into the array
+        index = Math.Clamp(index, 0, points.Length - 2);
+
+        // linear interpolation
+        float diff = (points[index + 1].value - points[index].value) / (points[index + 1].level - points[index].level);
+        float value = points[index].value + (level - points[index].level) * diff;
+
+        return value;
+    }
+};
+
+public enum DungeonPropertyKey {
+    // dungeon size (maybe expected number of rooms, or width)
+    Size,
+    // number of traps (maximum)
+    TrapCount,
+    // 
+    EnemyCount,
+    EnemyStrength,
+    // probability for having a boss
+    HasBoss,
+    // allows balancing of (scarce) resources 
+    AvailableXP,
+    AvailableGold,
+    AvailableAmmo
+}
+
