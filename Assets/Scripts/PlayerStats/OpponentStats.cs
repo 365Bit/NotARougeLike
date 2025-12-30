5371b5b05950e62959fa747a28a03be488c8ca74
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-30 15:25:00 +0100

    compute stat from opponent class definition and level

--- Assets/Scripts/PlayerStats/OpponentStats.cs
+++ Assets/Scripts/PlayerStats/OpponentStats.cs
@@ -25,6 +25,27 @@ public class OpponentStats : MonoBehaviour
     [Header("Perception")]
     public float memoryDuration;
     public float detectionRange;
+
+    public void ComputeFrom(OpponentClassDefinition def, int level) {
+        hitRate = def[OpponentStatKey.HitRate].ComputeFrom(level);
+		stunDuration = def[OpponentStatKey.StunDuration].ComputeFrom(level);
+
+		swingDuration = def[OpponentStatKey.SwingDuration].ComputeFrom(level);
+		strikeDuration = def[OpponentStatKey.StrikeDuration].ComputeFrom(level);
+		returnDuration = def[OpponentStatKey.ReturnDuration].ComputeFrom(level);
+		attackRange = def[OpponentStatKey.AttackRange].ComputeFrom(level);
+
+		maxHealth = def[OpponentStatKey.MaxHealth].ComputeFrom(level);
+		healthRegRate = def[OpponentStatKey.HealthRegRate].ComputeFrom(level);
+
+		wanderInterval = def[OpponentStatKey.WanderInterval].ComputeFrom(level);
+		wanderRadius = def[OpponentStatKey.WanderRadius].ComputeFrom(level);
+		rotationSpeed = def[OpponentStatKey.RotationSpeed].ComputeFrom(level);
+		movementSpeed = def[OpponentStatKey.MovementSpeed].ComputeFrom(level);
+
+		memoryDuration = def[OpponentStatKey.MemoryDuration].ComputeFrom(level);
+		detectionRange = def[OpponentStatKey.DetectionRange].ComputeFrom(level);
+    }
 }
 
 public enum OpponentStatKey {
