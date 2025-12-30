84cccb0c567d33ef4dc76eb064a65f645907d5e5
Axel Schneewind <axel.schneewind@protonmail.com>
2025-12-30 15:26:09 +0100

    add accessors

--- Assets/Scripts/PlayerStats/OpponentDefinitions.cs
+++ Assets/Scripts/PlayerStats/OpponentDefinitions.cs
@@ -4,11 +4,17 @@ public class OpponentDefinitions : MonoBehaviour
 {
     public OpponentClassDefinition[] classes;
 
+    public OpponentClassDefinition this[int c] {
+        get => classes[c];
+    }
+
     void Awake() {
         // normalize probabilities
         float probabilitySum = 0.0f;
-        foreach(OpponentClassDefinition def in classes)
+        foreach(OpponentClassDefinition def in classes) {
+            def.stats.Init();
             probabilitySum += def.spawnProbability;
+        }
 
         foreach(OpponentClassDefinition def in classes)
             def.spawnProbability /= probabilitySum;
@@ -22,5 +28,9 @@ public class OpponentClassDefinition {
     public GameObject prefab;
     [Tooltip("Defines scaling of stats in the opponent level")]
     public KeyValueStore<OpponentStatKey, InterpolationScaling> stats;
+
+    public InterpolationScaling this[OpponentStatKey key] {
+        get => stats[key];
+    }
 };
 
